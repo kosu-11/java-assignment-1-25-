e049798ac466fffe382aa51173715ea596c611c9
Kaushal Kumar <kosu1628015@gmail.com>
2025-11-18 11:19:34 +0530

    Add files via upload

---
+++ NumberUtils.java
@@ -0,0 +1,10 @@
+public class NumberUtils {
+
+    public int isEven(int number) {
+        if (number % 2 == 0) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+}
